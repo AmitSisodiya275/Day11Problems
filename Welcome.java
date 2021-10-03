07ddb6daef2c530883e7e84bef31fbdb3a778a27
Amit Sisodiya <amitsisodiya275@gmail.com>
2021-10-03 15:20:37 +0530

    Added: a new Welcome file which will print welcome message on the console.

---
+++ Welcome.java
@@ -0,0 +1,10 @@
+package com.bridgelab.stockaccountmanagement;
+
+public class Welcome {
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to Stock Account Management Project.");
+	}
+
+}
