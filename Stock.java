0156cf22114e599a77c6e4815d7b91d624511214
Amit Sisodiya <amitsisodiya275@gmail.com>
2021-10-03 15:23:25 +0530

    Added: a two new file that can calculate share value and whole stock value.

---
+++ Stock.java
@@ -0,0 +1,23 @@
+package com.bridgelab.stockaccountmanagement;
+
+public class Stock {
+
+	private String shareName;
+	private int numberOfShares;
+	private double sharePrice;
+	double totalStockPrice;
+
+	public Stock(String shareName, int numberOfShares, double sharePrice) {
+		this.shareName = shareName;
+		this.numberOfShares = numberOfShares;
+		this.sharePrice = sharePrice;
+		totalStockPrice = (double)numberOfShares * (double)sharePrice;
+	}
+
+	@Override
+	public String toString() {
+		return "Stock [stockName=" + shareName + ", numberOfShares=" + numberOfShares + ", sharePrice=" + sharePrice
+				+ ", totalStockPrice=" + totalStockPrice + "]";
+	}
+
+}
